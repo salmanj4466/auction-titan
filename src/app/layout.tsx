bf7f1e09ab3eef9688eebae5cfe63e7ad0d01141
ammarjavaid <ammarjavaid014@gmail.com>
2024-08-04 16:39:33 +0500

    setup variables and bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/globals.scss";
 import Header from "./Components/Common/Header";
 import Footer from "./Components/Common/Footer";
 
