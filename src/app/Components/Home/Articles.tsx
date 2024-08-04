2f2eea9256d1732ee62b3472ac181d6872218ed8
ammarjavaid <ammarjavaid014@gmail.com>
2024-08-05 01:48:07 +0500

    articles

--- src/app/Components/Home/Articles.tsx
+++ src/app/Components/Home/Articles.tsx
@@ -1,7 +1,56 @@
+import { Col, Row } from "antd";
+import "../../styles/globals.scss";
+
 const Articles = () => {
   return (
     <>
-      <p>Articles</p>
+      <div className="articles">
+        <div className="container articles-content">
+          <h2>Nuestros artículos más leidos</h2>
+
+          <Row gutter={16}>
+            <Col span={8}>
+              <div className="article-card">
+                <img src="/assets/articles/article1.png" alt="" />
+                <h3>Título</h3>
+                <p>
+                  Nulla vel est sed felis venenatis tempus ullamcorper vel leo.
+                  In varius odio et tristique tincidunt. Donec ut velit
+                  consequat...
+                </p>
+
+                <a href="/">Lee más </a>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="article-card">
+                <img src="/assets/articles/article2.png" alt="" />
+                <h3>Título</h3>
+                <p>
+                  Nulla vel est sed felis venenatis tempus ullamcorper vel leo.
+                  In varius odio et tristique tincidunt. Donec ut velit
+                  consequat...
+                </p>
+
+                <a href="/">Lee más </a>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div className="article-card">
+                <img src="/assets/articles/article3.png" alt="" />
+                <h3>Título</h3>
+                <p>
+                  Nulla vel est sed felis venenatis tempus ullamcorper vel leo.
+                  In varius odio et tristique tincidunt. Donec ut velit
+                  consequat...
+                </p>
+
+                <a href="/">Lee más </a>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import WorldTopRanking from "./Components/Common/WorldTopRanking";
+import Articles from "./Components/Home/Articles";
 import Hero from "./Components/Home/Hero";
 import LiveTournament from "./Components/Home/LiveTournament";
 import Newsletter from "./Components/Home/Newsletter";
@@ -6,10 +7,11 @@ import Newsletter from "./Components/Home/Newsletter";
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Newsletter />
-      <LiveTournament />
-      <WorldTopRanking />
+      {/* <Hero /> */}
+      {/* <Newsletter /> */}
+      <Articles />
+      {/* <LiveTournament /> */}
+      {/* <WorldTopRanking /> */}
     </main>
   );
 }
